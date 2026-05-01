6577c719205b96f9aec5ccf3381e0467161bf765
masashi inoue <jofidtv@gmail.com>
2026-05-01 15:01:33 +0900

    ui作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import styles from "./App.module.scss"
 import Header from "./components/header/Header"
 import TaskForm from "./features/task/taskForm/TaskForm"
-
+import TaskList from "./features/task/taskList/TaskList";
+import styles from "./App.module.scss"
 
 const App:React.FC=()=>{
   
@@ -11,6 +11,7 @@ const App:React.FC=()=>{
         <div className={styles.wrapper}>
           <Header/>
           <TaskForm/>
+          <TaskList />
         </div>
         
       </div>
